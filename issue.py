d17a96cc602c6d5fc59774e4b3b795c69f56b762
不留 <weinibuliu@outlook.com>
2024-12-11 00:22:24 +0800

    Update issue.py

--- issue.py
+++ issue.py
@@ -15,7 +15,7 @@ repo = Github(login_or_token=token).get_repo("weinibuliu/LowerArknightsGameData"
 def open():
     repo.create_issue(
         title=f"{lang} didn't build",
-        body=f"{lang} didn't build,please check Github Action.\nhttps://github.com/weinibuliu/LowerArknightsGameData/actions/runs/{action_id}  ({action_number})\n@weinibuliu",
+        body=f"{lang} didn't build,please check Github Action.\nhttps://github.com/weinibuliu/LowerArknightsGameData/actions/runs/{action_id}  ({action_num})\n@weinibuliu",
         labels=f"bug: {lang}",
     )
 
@@ -23,7 +23,7 @@ def open():
 def close():
     all_issues = repo.get_issues(state="open", labels=[f"bug: {lang}"])
 
-    issues: List[Issue.Issue] = List()
+    issues: List[Issue.Issue] = []
     new_issue = []
     num = 0
     while new_issue == [] and num <= 5:
@@ -33,7 +33,8 @@ def close():
         num += 1
 
     for issue in issues:
-        print(issue)
+        print(type(issue), issue)
+        print(issue.body)
         issue.create_comment(
             f"According to the operation of https://github.com/weinibuliu/LowerArknightsGameData/actions/runs/{action_id}  ({action_num}), the data building of {lang} has returned to normal.\nTherefore, this issue will be marked as completed and closed."
         )
