2d90f5cb34c33a3a0d11f4352c9fab7d5a3268e5
不留 <weinibuliu@outlook.com>
2024-12-26 00:41:02 +0800

    update

--- checksums.py
+++ checksums.py
@@ -11,12 +11,18 @@ gh = Github(login_or_token=token, retry=None)
 repo = gh.get_repo("weinibuliu/LowerArknightsGameData")
 release = repo.get_latest_release()
 assets = release.assets
+
 for a in assets:
     if ".zip" not in a.name:
         continue
-    with open(Path(Path.cwd(), "assets", a.name), "rb") as file:
+
+    path = Path(Path.cwd(), "assets", a.name)
+    with open(path, "rb") as file:
         data = file.read()
-        md5 = hashlib.md5().hexdigest()
-        print(Path(Path.cwd(), "assets", a.name))
-        print(f"{a.name}: {md5}")
+
+    md5 = hashlib.md5(data).hexdigest()
+    sha256 = hashlib.sha256(data).hexdigest()
+    print(f"{a.name} Path: {path}")
+    print(f"{a.name}: {md5}")
+
     a.update_asset(a.name, md5)
